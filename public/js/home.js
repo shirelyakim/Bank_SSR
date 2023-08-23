727a20dc863e40f221ee040dc61aa131b2527c72
Shaked Shalom <shaked201098@email.com>
2023-08-23 20:53:23 +0300

    more notes

--- public/js/home.js
+++ public/js/home.js
@@ -1,20 +1,20 @@
 async function validateUsername(element){
-    const username = element.value;
+    const username = element.value; 
     try {
-        user = await axios.get(`http://localhost:5000/api/users?userName=${username}`);
+        user = await axios.get(`http://localhost:5000/api/users?userName=${username}`); // Fetch user data from the server.
         if (user.data[0].disabled){
-            element.setCustomValidity("Username doesn't exists!")
-            element.reportValidity()
+            element.setCustomValidity("Username doesn't exists!") // Set a custom validation message if the username is disabled.
+            element.reportValidity() // Report the validation message to the user.
         }else{
-            element.setCustomValidity("")
-            element.reportValidity()
+            element.setCustomValidity("") // Reset custom validation message
+            element.reportValidity() // Report that the input is valid.
         }
     } catch{
         element.setCustomValidity("Username doesn't exists!")
         element.reportValidity()
     }
     const form = document.getElementById("createTransaction");
-    const current_user = form.username.value;
+    const current_user = form.username.value; // Get the current user's username from the form.
     if (username === current_user){
         element.setCustomValidity("Current username is not allowed!")
         element.reportValidity()
@@ -56,9 +56,11 @@ async function newTransaction(event){
 }
 
 async function filter() {
+    // Get the input elements
     const fromDateInput = document.getElementById("fromDate");
     const toDateInput = document.getElementById("toDate");
-
+    // Check if from date is empty
+    
     if (fromDateInput.value==="") {
       fromDateInput.setCustomValidity("Must insert");
       fromDateInput.reportValidity();
@@ -67,7 +69,8 @@ async function filter() {
       fromDateInput.setCustomValidity("");
       fromDateInput.reportValidity();
     }
-  
+
+    // Check if to date is empty
     if (toDateInput.value==="") {
       toDateInput.setCustomValidity("Must insert");
       toDateInput.reportValidity();
@@ -76,10 +79,13 @@ async function filter() {
       toDateInput.setCustomValidity("");
       toDateInput.reportValidity();
     }
+     // Convert from and to dates to Date objects and set time to start and end of day
     let fromDate = new Date(fromDateInput.value)
     fromDate = fromDate.setHours(0,0,0,0)
     let toDate = new Date(toDateInput.value)
     toDate = toDate.setHours(23,59,59,59)
+
+     // Check if from date is greater than to date
     if (fromDate > toDate){
         toDateInput.setCustomValidity("To date must be bigger");
         toDateInput.reportValidity();
@@ -89,7 +95,7 @@ async function filter() {
         toDateInput.setCustomValidity("");
         toDateInput.reportValidity();
     }
-
+    // Initialize arrays to store transactions
     let transactions = []
     let outTransactions
     let inTransactions
@@ -99,25 +105,30 @@ async function filter() {
     } catch {outTransactions = []}
     try{ inTransactions = (await axios.get(`http://localhost:5000/api/transactions?destenationUserID=${user_id}`)).data
     } catch {inTransactions = []}
+    // Create a dictionary/ an object of users using their IDs as keys
     var users = {};
     let rawUsers = (await axios.get(`http://localhost:5000/api/users`)).data
     for (let i = 0; i < rawUsers.length; i++) {
         users[rawUsers[i]._id] = rawUsers[i] 
     }
+    // Filter and format transactions based on date range
     for (let i = 0; i < outTransactions.length; i++) {
         let transactionDate = new Date(outTransactions[i].date)
         if(transactionDate <= toDate && transactionDate >= fromDate){
-            transactions.push({"id": outTransactions[i]._id,"date": new Date(outTransactions[i].date), "amount": `-${outTransactions[i].amount}`, "username": users[`${outTransactions[i].destenationUserID}`].userName})
+            transactions.push({"id": outTransactions[i]._id,"date": new Date(outTransactions[i].date),
+             "amount": `-${outTransactions[i].amount}`, "username": users[`${outTransactions[i].destenationUserID}`].userName})
         }
     }
     for (let i = 0; i < inTransactions.length; i++) {
         let transactionDate = new Date(inTransactions[i].date)
         if(transactionDate <= toDate && transactionDate >= fromDate){
-            transactions.push({"id": inTransactions[i]._id,"date": new Date(inTransactions[i].date), "amount": `+${inTransactions[i].amount}`, "username": users[`${inTransactions[i].sourceUserID}`].userName})
+            transactions.push({"id": inTransactions[i]._id,"date": new Date(inTransactions[i].date),
+             "amount": `+${inTransactions[i].amount}`, "username": users[`${inTransactions[i].sourceUserID}`].userName})
         }
     }
+    // Sort transactions by date in descending order
     transactions = transactions.sort(function(a,b){return b.date - a.date;});
-
+    // Update the transactions table in the HTML
     var transactionsTableBody = document.getElementById("transactionsTable");
     transactionsTableBody.innerHTML = "";
     for (var i = 0; i < transactions.length; i++) {
